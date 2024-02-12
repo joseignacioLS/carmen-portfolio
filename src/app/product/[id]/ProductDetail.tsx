6b376992f0998eecae90298ad1f6970deaac3134
Jose <ls.joseignacio@gmail.com>
2024-02-12 12:12:39 +0100

    Update ProductDetail.tsx

--- src/app/product/[id]/ProductDetail.tsx
+++ src/app/product/[id]/ProductDetail.tsx
@@ -3,19 +3,22 @@
 import { useParams } from "next/navigation";
 import styles from "./ProductDetail.module.scss";
 import { Products } from "@/data/products";
+import Carousel from "@/components/Carousel";
 
 const ProductDetail = () => {
   const { id } = useParams();
   const info = Products.find((entry) => entry.id === id);
   if (info === undefined) return <>Not Found</>;
   return (
-    <div>
+    <div className={styles.productDetail}>
       <h1 className={styles.title}>
         <span style={{ viewTransitionName: `title-${info.id}` }}>
           {info.name}
         </span>
       </h1>
-      <img className={styles.mainImage} src={info.image} />
+      <div className={styles.carousel}>
+        <Carousel images={[info.image]} />
+      </div>
     </div>
   );
 };
