3e430d0d6b971e2e8b087c3167d24ca6a89d75be
Jose <ls.joseignacio@gmail.com>
2024-02-13 15:19:24 +0100

    fix: localstorage init value language

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -9,11 +9,13 @@ const resources = {
 };
 
 
+const lng = typeof localStorage !== "undefined" ? JSON.parse(localStorage.getItem("lng") || '{"values":"es"}')?.value || "es" : "es"
+
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: JSON.parse(localStorage.getItem("lng") || '{"values":"es"}')?.value || "es", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    lng, // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
